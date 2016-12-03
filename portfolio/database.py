4c97ee5c382dc4c7acf9988bc850f70d5077a871
Dan Turner <dturner@tcd.ie>
2016-12-03 20:20:03 +0000

    Added the database skeleton and updated the requirements

---
+++ portfolio/database.py
@@ -0,0 +1,25 @@
+'''Database setup'''
+import pymongo
+
+
+class Database(object):
+    URI = 'mongodb://127.0.0.1:27017' # replace with raspi
+    DATABASE = None
+
+    @staticmethod
+    def init():
+        '''Setup the mongo database'''
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client['fullstack']
+
+    @staticmethod
+    def insert(collection, json):
+        Database.DATABASE[collection].insert(json)
+
+    @staticmethod
+    def find(collection, query):
+        Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        Database.DATABASE[collection].find_one(query)
