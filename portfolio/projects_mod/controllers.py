8068e378ad25a4cf5b0c176f666507dbbe9daa38
Dan Turner <dturner@tcd.ie>
2016-12-03 20:33:43 +0000

    updated to blueprints

---
+++ portfolio/projects_mod/controllers.py
@@ -0,0 +1,20 @@
+'''projects routes '''
+from portfolio import app
+
+@app.route('/projects/')
+def projects():
+    '''Projects Page'''
+    return 'Projects List'
+
+@app.route('/projects/<int:project_id>')
+def project(project_id):
+    '''Project_id page'''
+    return 'Project id : %d' % project_id
+
+
+@app.route('/template/')
+@app.route('/template/<test>')
+def template(test=None):
+    '''Template'''
+    return render_template('template.html', test=test)
+    
