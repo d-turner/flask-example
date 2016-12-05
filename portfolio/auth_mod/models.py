5bb9f37f517353aa86f2411aea5452dafae40a3e
Dan Turner <dturner@tcd.ie>
2016-12-05 13:58:28 +0000

    Added the user model to the authentication module

--- portfolio/auth_mod/models.py
+++ portfolio/auth_mod/models.py
@@ -1,27 +1,48 @@
 '''models auth interacts with'''
 __author__ = 'dturner'
 
+from portfolio.common.database import Database
+from flask import session
+import uuid 
+
 # Define a User model
 class User(object):
     '''User model {username, password, pass}'''
 
-    def __init__(self, name, email, password):
+    def __init__(self, name, email, password, _id=None):
         self.name = name
         self.email = email
         self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
 
-    def get_by_email(self):
-        pass
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one('users', {'email': email})
+        if data is not None:
+            return cls(**data)
 
-    def get_by_id(self):
-        pass
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one('users', {'_id': _id})
+        if data is not None:
+            return cls(**data)
 
-    def login_valid(self):
-        pass
+    @staticmethod
+    def login_valid(email, password):
+        user = User.get_by_email(email)
+        if user is not None:
+            # need to add in the salt algorithm here
+            return user.password == password
+        return False
 
+
+    @staticmethod
+    def login(user_email):
+        # Login valid 
+        session['email'] = user_email
+
+
+    @staticmethod
     def register(self):
-        pass
-
-    def login(self):
         pass
