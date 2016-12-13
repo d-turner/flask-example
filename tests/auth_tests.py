80316bbba9b8c9a6636c734287b238fd85f07b5b
Dan Turner <dturner@tcd.ie>
2016-12-13 19:53:01 +0000

    adjusted the assertion methods for all tests

--- tests/auth_tests.py
+++ tests/auth_tests.py
@@ -22,23 +22,23 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_register_get(self):
         response = self.app.get('/auth/register')
-        assert b'' in response.data, 'Get request failed'
+        self.assertEqual(response.data, '', 'Get request failed')
 
     def test_register_post(self):
         email = 'test@test.com'
         password = 'pass123'
         response = self.app.post('/auth/register', data=dict(
-            email=email, 
+            email=email,
             password=password), follow_redirects=True)
-        assert response.data, 'Post request failed'
+        self.assertEqual(response.data, 'not yet implemented', 'Post request failed')
 
     def test_login(self):
         response = self.app.get('/auth/login')
-        assert b'Hello World!' in response.data, 'Login request failed'
+        self.assertEqual(response.data, '', 'Login request failed')
 
     def test_forgot_password(self):
         response = self.app.post('/auth/forgot')
-        assert b'Hello World!' in response.data, 'Forgot request failed'
+        self.assertEqual(response.data, '', 'Forgot request failed')
 
 
 if __name__ == '__main__':
