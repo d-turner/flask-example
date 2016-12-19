7bba441ced22be1d8ee56c60a86bff548caabbcd
Dan Turner <dturner@tcd.ie>
2016-12-19 16:28:54 +0000

    Restructured the register route and refactored the template file names

--- portfolio/auth_mod/controllers.py
+++ portfolio/auth_mod/controllers.py
@@ -3,6 +3,7 @@ from flask import Blueprint, abort, redirect, render_template, request, url_for
 from jinja2 import TemplateNotFound
 
 import portfolio.constants as constants
+from portfolio.auth_mod.forms import LoginForm, RegisterForm
 from portfolio.auth_mod.models import User
 
 auth_mod = Blueprint('auth', __name__,
@@ -10,24 +11,24 @@ auth_mod = Blueprint('auth', __name__,
 
 @auth_mod.route('/auth/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'GET':
-        return render_template('auth/signup.html')
-    else:
-        email = request.form['email']
-        password = request.form['password']
-        #name = request.form['name']
-        if User.register_user(email, password):
-            return redirect(url_for('auth.login'))
+    register_form = RegisterForm()
+    if request.method == 'POST':
+        if register_form.validate_on_submit():
+            email = request.form['email']
+            password = request.form['password']
+            if User.register_user(email, password):
+                return redirect(url_for('index.home'))
         return render_template(
-            'auth/signup.html',
+            'auth/register.html', form=register_form,
             error=constants.EMAIL_IN_USE_ERROR)
+    return render_template('auth/register.html', form=register_form)
 
 
 @auth_mod.route('/auth/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         try:
-            return render_template('auth/signin.html')
+            return render_template('auth/login.html')
         except TemplateNotFound:
             abort(404)
     else:
